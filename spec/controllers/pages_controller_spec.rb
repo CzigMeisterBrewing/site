3b30b835354e3cecbfe8efd16a8ee566769a74bc
Carlos Lazo <carlos.jose.lazo@gmail.com>
2016-03-06 15:51:18 -0500

    Adds Rspec helper files and tests for PagesController.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,58 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #brewery" do
+    it "returns http success" do
+      get :brewery
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #faq" do
+    it "returns http success" do
+      get :faq
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe "GET #home" do
     it "returns http success" do
       get :home
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #tasting_room" do
+    it "returns http success" do
+      get :tasting_room
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #team" do
+    it "returns http success" do
+      get :team
+
       expect(response).to have_http_status(:success)
     end
   end
