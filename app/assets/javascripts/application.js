6b120b9bfb80524bbb539f8bc52c9365420d6ede
Carlos Lazo <carlos.jose.lazo@gmail.com>
2016-03-12 17:38:05 -0500

    Adds Bushido theme behavior / styling to the primary web layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,18 @@
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+// Load styling for Bushido template.
+
+//= require js/libs/jquery-1.11.1.min
+//= require js/libs/jquery-ui-1.10.4.custom.min
+//= require js/libs/jquery.easing.min
+//= require js/plugins/bootstrap.min
+//= require js/plugins/smoothscroll
+//= require js/plugins/jquery.validate.min
+//= require js/plugins/icheck.min
+//= require js/plugins/jquery.placeholder
+//= require js/plugins/jquery.stellar.min
+//= require js/plugins/jquery.touchSwipe.min
+//= require js/plugins/jquery.shuffle.min
+//= require js/plugins/lightGallery.min
+//= require js/plugins/owl.carousel.min
+//= require js/plugins/masterslider.min
+//= require mailer/mailer
+//= require js/scripts
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add in path for Bushido theme.
-Rails.application.config.assets.paths << Rails.root.join('app/vendor/assets/bushido')
+# Bushido Theme assets:
+Rails.application.config.assets.precompile += %w( js/libs/modernizr.custom.js )
 
-# Precompile additional assets.
+# Precompile additional assets:
 Rails.application.config.assets.precompile += ['controller/*']
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf)
