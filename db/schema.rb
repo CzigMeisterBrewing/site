339d667292793763334a0181895ae7bcd2e9ada9
Carlos Lazo <carlos.jose.lazo@gmail.com>
2016-10-16 18:59:02 -0400

    Add Event model and spec skeletons

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161016223715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.date     "date",        null: false
+    t.string   "link"
+    t.string   "location",    null: false
+    t.string   "name",        null: false
+    t.string   "time"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["date"], name: "index_events_on_date", using: :btree
+  end
 
 end
