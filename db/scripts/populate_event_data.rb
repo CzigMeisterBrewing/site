fcc38b84cfe3a95e7dc0cd861349d57d60b5cfb0
Carlos Lazo <carlos.jose.lazo@gmail.com>
2016-10-17 01:37:33 -0400

    Create migration script for current relevant events

---
+++ db/scripts/populate_event_data.rb
@@ -0,0 +1,37 @@
+require File.expand_path('../../../config/environment', __FILE__)
+
+Event.create!(
+  date:        Date.new(2016,10,18),
+  description: nil,
+  link:        'http://capitalcraftnj.com/',
+  location:    'Green Brook',
+  name:        'Capital Craft Tap Takeover',
+  time:        nil
+)
+
+Event.create!(
+  date:        Date.new(2016,10,23),
+  description: '$20 cash-only yoga followed by a free pint; email Larissa Harm at larissaharm89@gmail.com to preregister!',
+  link:        nil,
+  location:    'at the brewery',
+  name:        'Yoga on Tap',
+  time:        '10:30am'
+)
+
+Event.create!(
+  date:        Date.new(2016,10,27),
+  description: nil,
+  link:        'http://www.midlandbrewhouse.com/',
+  location:    'Saddle Brook',
+  name:        'Midland Brew House Tap Takeover',
+  time:        nil
+)
+
+Event.create!(
+  date:        Date.new(2016,10,29),
+  description: nil,
+  link:        nil,
+  location:    'at the brewery',
+  name:        'Czig Meister Halloween Celebration',
+  time:        nil
+)
