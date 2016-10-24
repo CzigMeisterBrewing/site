17547067b3ecabb732259cf4af2daec96d4b4cdd
Carlos Lazo <carlos.jose.lazo@gmail.com>
2016-10-24 07:48:03 -0400

    Update Event list

--- db/scripts/populate_event_data.rb
+++ db/scripts/populate_event_data.rb
@@ -1,24 +1,6 @@
 require File.expand_path('../../../config/environment', __FILE__)
 
 Event.create!(
-  date:        Date.new(2016,10,18),
-  description: nil,
-  link:        'http://capitalcraftnj.com/',
-  location:    'Green Brook',
-  name:        'Capital Craft Tap Takeover',
-  time:        nil
-)
-
-Event.create!(
-  date:        Date.new(2016,10,23),
-  description: '$20 cash-only yoga followed by a free pint; email Larissa Harm at <a href="mailto:larissaharm89@gmail.com?subject=Yoga%20on%20Tap%20Registration", target="_blank"> larissaharm89@gmail.com</a> to preregister!',
-  link:        nil,
-  location:    'at the brewery',
-  name:        'Yoga on Tap',
-  time:        '10:30am'
-)
-
-Event.create!(
   date:        Date.new(2016,10,27),
   description: nil,
   link:        'http://www.midlandbrewhouse.com/',
