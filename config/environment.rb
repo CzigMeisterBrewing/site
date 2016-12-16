95126f1399b719435b9d17bb70bae2ac3f6c0fbd
Carlos Lazo <carlos.jose.lazo@gmail.com>
2016-12-15 19:35:15 -0500

    Move ActionMailer config to production env

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,4 @@ require_relative 'application'
 Rails.application.initialize!
 
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    authentication:       'plain',
-    domain:               'czigmeisterbrewing.com',
-    enable_starttls_auto: true,
-    password:             ENV['GMAIL_PASS'],
-    port:                 587,
-    user_name:            ENV['GMAIL_USER']
-  }
 end
